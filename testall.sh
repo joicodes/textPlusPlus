21d3f4d875f3ad2f6f3fa08f22f64f26faed924e
Joi Anderson <joicodes@gmail.com>
2018-11-28 12:36:44 -0500

    edited testall

--- testall.sh
+++ testall.sh
@@ -94,7 +94,7 @@ Check() {
     generatedfiles="$generatedfiles ${basename}.ll ${basename}.s ${basename}.exe ${basename}.out" &&
     Run "$TEXTPLUSPLUS" "$1" ">" "${basename}.ll" &&
     Run "$LLC" "-relocation-model=pic" "${basename}.ll" ">" "${basename}.s" &&
-    Run "$CC" "-o" "${basename}.exe" "${basename}.s" "hello.o" &&
+    Run "$CC" "-o" "${basename}.exe" "${basename}.s" "hello.o " "-lhpdf" &&
     Run "./${basename}.exe" > "${basename}.out" &&
     Compare ${basename}.out ${reffile}.out ${basename}.diff
 
