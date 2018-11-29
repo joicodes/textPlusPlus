c36d213a815d263eaac6bfb6d1b5f65b3749a16b
Joi Anderson <joicodes@gmail.com>
2018-11-29 17:47:13 -0500

    moved message to top of the page

--- hello.c
+++ hello.c
@@ -66,7 +66,7 @@ int  addPage( int num){
 
 int write( char * num){
     const char *text = "Hello World";
-    HPDF_Page_TextOut (currentPage, currentX, 500, num);
+    HPDF_Page_TextOut (currentPage, currentX, currentY-currentSize, num);
     return 0;
 
 }
