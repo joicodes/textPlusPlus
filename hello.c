97b6fbf3db6d2ea06c8281a8500bf85d1bf68e65
Joi Anderson <joicodes@gmail.com>
2018-12-01 22:18:42 -0500

    fixed write function to write on more than one page

--- hello.c
+++ hello.c
@@ -61,12 +61,11 @@ int  addPage( int num){
     HPDF_Page newPage;
     newPage = HPDF_AddPage(pdf);
 
-    HPDF_Page_EndText(currentPage);
-
     currentPage = newPage;
-    HPDF_Page_BeginText (currentPage);
 
     pageNumber = pageNumber + 1;
+
+    HPDF_Page_SetFontAndSize(currentPage, currentFont, currentSize);
 
     currentX = 0;
     currentY = pageHeight;
@@ -76,9 +75,13 @@ int  addPage( int num){
 }
 
 
-int write( char * num){
-    const char *text = "Hello World";
-    HPDF_Page_TextOut (currentPage, currentX, currentY-currentSize, num);
+int write( char * text){
+    const char *sample = "Hello World";
+
+    HPDF_Page_BeginText(currentPage);
+    HPDF_Page_TextOut (currentPage, currentX, currentY-currentSize, text);
+    HPDF_Page_EndText(currentPage);
+
     return 0;
 
 }
@@ -90,36 +93,29 @@ int bold( int num ){
 
 	if ((currentFont == helvetica) || (currentFont == helveticaItalic)){
 		
-		HPDF_Page_EndText(currentPage);
 
 		currentFont = helveticaBold;
 		HPDF_Page_SetFontAndSize(currentPage, currentFont, currentSize);
-
-		HPDF_Page_BeginText(currentPage);	
+	
 	}
 	
 	// Times Bold
 
 	if ((currentFont == times) || (currentFont == timesItalic)){
 
-		HPDF_Page_EndText(currentPage);
-
 		currentFont = timesBold;
 		HPDF_Page_SetFontAndSize(currentPage, currentFont, currentSize);
 
-		HPDF_Page_BeginText(currentPage);
 	}
 
 	// Courier Bold
 
 	if ((currentFont == courier) || (currentFont == courierItalic)){
 
-		HPDF_Page_EndText(currentPage);
 
 		currentFont = courierBold;
 		HPDF_Page_SetFontAndSize(currentPage, currentFont, currentSize);
 
-		HPDF_Page_BeginText(currentPage);
 	}
 
 	return 0;
@@ -132,36 +128,25 @@ int italic( int num ){
 
 	if ((currentFont == helvetica) || (currentFont == helveticaBold)){
 		
-		HPDF_Page_EndText(currentPage);
-
 		currentFont = helveticaItalic;
 		HPDF_Page_SetFontAndSize(currentPage, currentFont, currentSize);
-
-		HPDF_Page_BeginText(currentPage);	
 	}
 	
 	// Times Italic
 
 	if ((currentFont == times) || (currentFont == timesBold)){
 
-		HPDF_Page_EndText(currentPage);
-
 		currentFont = timesItalic;
 		HPDF_Page_SetFontAndSize(currentPage, currentFont, currentSize);
 
-		HPDF_Page_BeginText(currentPage);
 	}
 
 	// Courier Italic
 
 	if ((currentFont == courier) || (currentFont == courierBold)){
 
-		HPDF_Page_EndText(currentPage);
-
 		currentFont = courierItalic;
 		HPDF_Page_SetFontAndSize(currentPage, currentFont, currentSize);
-
-		HPDF_Page_BeginText(currentPage);
 	}
 
 	return 0;
@@ -174,37 +159,28 @@ int regular( int num ){
 	// Helvetica 
 
 	if ((currentFont == helveticaItalic) || (currentFont == helveticaBold)){
-		
-		HPDF_Page_EndText(currentPage);
 
 		currentFont = helvetica;
 		HPDF_Page_SetFontAndSize(currentPage, currentFont, currentSize);
 
-		HPDF_Page_BeginText(currentPage);	
 	}
 	
 	// Times Italic
 
 	if ((currentFont == timesItalic) || (currentFont == timesBold)){
 
-		HPDF_Page_EndText(currentPage);
-
 		currentFont = times;
 		HPDF_Page_SetFontAndSize(currentPage, currentFont, currentSize);
-
-		HPDF_Page_BeginText(currentPage);
 	}
 
 	// Courier Italic
 
 	if ((currentFont == courierItalic) || (currentFont == courierBold)){
 
-		HPDF_Page_EndText(currentPage);
-
 		currentFont = courier;
 		HPDF_Page_SetFontAndSize(currentPage, currentFont, currentSize);
 
-		HPDF_Page_BeginText(currentPage);
+
 	}
 
 	return 0;
@@ -256,7 +232,7 @@ int main(int argc)
     currentX = 0;
     currentY = pageHeight;
 
-    HPDF_Page_BeginText (currentPage);
+    //HPDF_Page_BeginText (currentPage);
 
 
     //HPDF_Page_TextOut (currentPage, 60, 500, page_title); // prints text in specified position
@@ -269,7 +245,7 @@ int main(int argc)
 
 
     // Ends program
-    HPDF_Page_EndText (currentPage);
+    //HPDF_Page_EndText (currentPage);
     HPDF_SaveToFile (pdf, "text.pdf");
     HPDF_Free (pdf);
 
